d961c012a9ca350f361b6fe194a868d04c9cfb56
Nation <shakyanation@gmail.com>
2019-07-18 14:33:30 +0545

    Budget inserted and retrieved from database

--- app/src/main/java/com/example/merowalletv11/PromptActivity.java
+++ app/src/main/java/com/example/merowalletv11/PromptActivity.java
@@ -10,16 +10,33 @@ import android.widget.Toast;
 public class PromptActivity extends AppCompatActivity {
 
     private static double budget=0;
+    DatabaseHelper MDb;
+    private static String username;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prompt);
+        username = LoginActivity.throwUsername();
+        MDb = new DatabaseHelper(this);
     }
 
     public void buttonBudget(View view) {
 
         EditText edt=findViewById(R.id.editTextDialogUserInput);
         budget=Double.parseDouble(edt.getText().toString());
+
+        boolean isUpdate = MDb.updateBudget(username,budget);
+        if(isUpdate == true){
+            Toast.makeText(PromptActivity.this,"Budget updated",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(PromptActivity.this,"Budget update failed",Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+
         Intent in = new Intent(PromptActivity.this, MainActivity.class);
         startActivity(in);
     }
